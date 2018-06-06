b97d730432e7115d295803e8f179b162b78cd379
Volodymyr <34250821+VoBilyk@users.noreply.github.com>
2018-06-06 18:30:17 +0300

    added AzureBot project

---
+++ SudokuBot/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=testbilykbot-Web-Deploy.pubxml -p:Password=9Ez9mYbKKWk3wSjbiTrFFkwF5DmdbehAdi8r2DG7kQoZYEHiChoZ9TsLZcnZ
+
